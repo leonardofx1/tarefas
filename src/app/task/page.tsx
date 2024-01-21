9a47643d80e2e2cb9efd229b76490eb040942837
leonardo <102767490+leonardofx1@users.noreply.github.com>
2024-01-21 20:10:20 -0300

    task style

--- src/app/task/page.tsx
+++ src/app/task/page.tsx
@@ -1,5 +1,6 @@
 import styles from './task.module.scss'
-
+import { FiTrash } from "react-icons/fi";
+import { FaShare } from "react-icons/fa";
 
 const Task = () => {
 
@@ -8,7 +9,7 @@ const Task = () => {
      <main className={styles.main}>
         <section className={styles.containerCreateTask}>
             <h2 className={styles.titulo}>Qual  sua tarefa? </h2>
-            <textarea className={styles.textArea} name="" id="" cols="30" rows="10"></textarea>
+            <textarea className={styles.textArea} ></textarea>
             <div>
             <input type="checkbox" name="" id="" />
             <span>Deixar tarefa pública</span>
@@ -17,7 +18,35 @@ const Task = () => {
         </section>
         <section className={styles.containerTask}>
             <h2>Minhas tarefas</h2>
-
+            <article className={styles.bodyTask}>
+                
+                    <button>
+                        Publica <FaShare color=' #0f2f4d'/>
+                    </button>
+                    <div>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque qui sed consequatur, consectetur in praesentium animi tempora dolorem eligendi omnis, harum et sequi obcaecati inventore velit. Laborum error fugiat Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae illum consectetur voluptatum esse, aspernatur hic, quam exercitationem quos expedita aliquam odit omnis a unde. Facere nemo tempora harum ratione! Omnis?recusandae.</p><span><FiTrash size={29} /></span>
+                    </div>
+            
+            </article>
+            <article className={styles.bodyTask}>
+                
+                <button>
+                    Publica <FaShare color=' #0f2f4d'/>
+                </button>
+                <div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque qui sed consequatur, consectetur in praesentium animi tempora dolorem eligendi omnis, harum et sequi obcaecati inventore velit. Laborum error fugiat Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae illum consectetur voluptatum esse, aspernatur hic, quam exercitationem quos expedita aliquam odit omnis a unde. Facere nemo tempora harum ratione! Omnis?recusandae.</p><span><FiTrash size={29} /></span>
+                </div>
+        
+        </article>  <article className={styles.bodyTask}>
+                
+                <button>
+                    Publica <FaShare color=' #0f2f4d'/>
+                </button>
+                <div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque qui sed consequatur, consectetur in praesentium animi tempora dolorem eligendi omnis, harum et sequi obcaecati inventore velit. Laborum error fugiat Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae illum consectetur voluptatum esse, aspernatur hic, quam exercitationem quos expedita aliquam odit omnis a unde. Facere nemo tempora harum ratione! Omnis?recusandae.</p><span><FiTrash size={29} /></span>
+                </div>
+        
+        </article>
         </section>
      </main>
     )
